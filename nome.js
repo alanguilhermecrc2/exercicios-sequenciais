03f2d35c822d2fe65da01ceeb1c205d6d315976d
alanguilhermedw <alanguilhermedw@gmail.com>
2023-03-06 12:09:46 -0300

    first commit

---
+++ nome.js
@@ -0,0 +1,87 @@
+/*
+console.log(); -> mostrar no terminal
+var
+let
+const
+
+"" `` '' --> string
+
+if (condicao) {
+    "faz tal coisa se a condição for verdade, se for mentira vai pro else" 
+} else {
+    "faz tal coisa"
+}
+
+*/
+
+//Calcule a média de duas notas, se a média for maior ou igual a 7 mostre que ele está aprovado, se for menor reprovado;
+
+// let n1 = 6
+// let n2 = 8
+// let media = (n1+n2)/2
+
+// if (media >= 7) {
+    //     console.log("aluno aprovado")
+    // } else {
+        //  console.log("aluno reprovado")
+        // }
+        
+        // let n1 = 2
+        // for (let i = 0; i <= 10; i++) {
+            //     console.log(i)
+// }
+
+// let tabuada = 5
+// for (let i= 0; i <=10; i++){
+//     console.log(`${tabuada} x ${i}= ${tabuada*i}`)
+// }
+
+// let tabuada = 8
+// for (let i=0; i <=10; i++){
+    //     console.log(`${tabuada} + ${i}= ${tabuada+i}`)
+    // }
+    
+    
+    // let nota = [12,8,18];
+    // let media = (nota[0]+nota[1]+nota[2])/nota.length
+    // console.log(`media é ${media}`)
+    
+// let A = 8
+// let B = 16
+// //
+// console.log("Antes",A,B);
+
+// let temp = A;
+// A = B;
+// B = temp
+// console.log("Depois",A,B);
+// let Salário = 1200
+// let aumento = Salário *(25/100)
+// console.log("novo salario", Salário + aumento)
+// let nascimento= 2003
+// let atual = 2023
+// console.log('idade atual =', atual-nascimento)
+// console.log('idade em 2010 =',2010- nascimento)
+// let nascimento = 1980
+// let atual = 2023
+// console.log('idade em anos =', atual-nascimento)
+// console.log("idade em meses=", 12* (atual-nascimento))
+// console.log('idade em dias=' ,365* (atual-nascimento))
+// console.log( 'idade da pessoa em semanas=', 52* (atual-nascimento))
+// let peso = 70
+// let altura = 1.80
+// let calculo = peso / (altura*altura)
+// console.log("IMC =", calculo)
+// let grauC= 28
+// console.log("temperatura em Fahrenheit =", grauC*1.8+32)
+// let pedro= 80
+// console.log('o peso de pedro em gramas é = ', pedro*(100))
+// let min= 350
+// let convert = min/60
+// console.log('350min em horas é =', convert)
+let nome = "angela"
+let idade = 20
+let dias = idade*365
+console.log(nome,', VOCÊ JÁ VIVEU', dias, "dias")
+console.log(`${nome}, voce já viveu ${dias} dias`)
+let
